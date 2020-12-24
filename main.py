ef8361f4b27d7b199871a04fa2b9bb6bf4838346
312585 <kate_01222@mail.ru>
2020-12-24 18:25:33 +0300

    version2.2

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ WIDTH = 600
 file = open('result.bmp', 'wb')
 # необходимые для формата два заголовка на 14 и 40 байт и далее 8 байт цветовой карты(созданы два цвета-черный и белый)
 file.write(b'BM')
-file.write(pack('<LHHL', 62 + HEIGHT * WIDTH, 0, 0, 62))
+file.write(pack('<LHHL', 62 + HEIGHT*WIDTH, 0, 0, 62))
 file.write(pack('<LLLHHLLLLLL', 40, HEIGHT, WIDTH, 1, 8, 0, 0, 0, 0, 0, 0))
 color_0 = (0, 0, 0, 0)
 color_1 = (255, 255, 255, 0)
@@ -17,8 +17,8 @@ file.write(pack('<8B', *color_0, *color_1))
 
 # создание параметрической функции
 t = np.linspace(0, 2 * np.pi, 600)
-x = 6 * np.cos(t) - 4 * (np.cos(t)) ** 3
-y = 4 * (np.sin(t)) ** 3
+x = 6*np.cos(t) - 4*(np.cos(t))**3
+y = 4 * (np.sin(t))**3
 # округляем значения
 x = [round(v, 5) for v in x]
 y = [round(v, 5) for v in y]
@@ -41,7 +41,7 @@ for i in y:
     # создаем список значений где числовое значение х приводим к номеру пикселя
     x_pixels = []
     for cur_y in good_x:
-        x_pixels.append(math.trunc(cur_y * 106 + 300))
+        x_pixels.append(math.trunc(cur_y*106 + 300))
     # проходимся в ширину и красим пиксели выбранные ранее в черный, остальные в белый
     for a in range(WIDTH):
         if a in x_pixels:
@@ -49,4 +49,3 @@ for i in y:
         else:
             file.write(pack('<B', 1))
 file.close()
-
